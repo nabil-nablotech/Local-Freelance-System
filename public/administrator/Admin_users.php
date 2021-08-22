628aa4fef33572add5e429b58cdb682e96d525d4
muleA <56386485+muleA@users.noreply.github.com>
2021-08-22 10:14:54 +0300

    admin  user jquery src changed

--- public/administrator/Admin_users.php
+++ public/administrator/Admin_users.php
@@ -4,8 +4,9 @@
    ?>
 <script>
     document.title="Admin-list of system admins";
-</script> <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+</script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
 integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
 crossorigin="anonymous"></script>   
 <!-- content begins -->
@@ -193,6 +194,7 @@ crossorigin="anonymous"></script>
 <a class="scroll-to-top rounded" href="#page-top">
 <i class="fas fa-angle-up"></i>
 </a>
+
 <script src="../assets/vendor/jquery/jquery.min.js"></script>  
 <script src="../assets/vendor/datatables/jquery.dataTables.js" ></script>
 <script src="../assets/vendor/datatables/jquery.dataTables.min.js" ></script>
@@ -201,6 +203,7 @@ crossorigin="anonymous"></script>
 <script src="../assets/js/administrator/serelance-admin.js "></script>
 <script src="../assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <script src="../assets/vendor/datatables/dataTables.bootstrap4.js" ></script>
+
 <script type = "text/javascript">
    $(document).ready(function(){
    	$("#table").DataTable();
