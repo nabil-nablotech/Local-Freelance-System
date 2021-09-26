330099903dce6e26df02da210a2150240e44d68f
Nabil Mohammed <nabil.nablotech@gmail.com>
2021-09-27 02:21:54 +0300

    Modified .htaccess for routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
-    $_SESSION['baseurl'] = "http://192.168.1.3/seralance/";
+    //$_SESSION['baseurl'] = "https://hostprovider.com/";
+
+    /* When in local dev env */
+    $_SESSION['baseurl'] = "http://localhost/seralance/";
     require_once('../app/init.php');
     $app = new App;    
 ?>
